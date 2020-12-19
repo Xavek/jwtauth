88956a46b85a68e5ad8768b8b89d119313dd3aac
gautam2002 <theofficalgautam200@gmail.com>
2020-12-19 17:25:16 +0545

    added user data validation

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,18 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authenticate = require("../middlewares/authenticate");
 require("dotenv").config();
+const Joi = require("joi");
+// User Data Validation
+const validateSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  password: Joi.string().alphanum().min(8).max(20).required(),
+});
 
 router.route("/register").post((req, res) => {
+  const { error } = validateSchema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
   bcrypt.hash(req.body.password, 10, (err, hashedPass) => {
     if (err) {
       res.json(err);
